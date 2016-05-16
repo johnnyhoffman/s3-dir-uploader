0df2412d8dafc6e40fb87c92a9c452dfc055c4d8
Johnny Hoffman <johnnyhoffman33@gmail.com>
2016-05-15 19:02:29 -0700

    from lein new template

---
+++ test/s3_dir_uploader/core_test.clj
@@ -0,0 +1,7 @@
+(ns s3-dir-uploader.core-test
+  (:require [clojure.test :refer :all]
+            [s3-dir-uploader.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
